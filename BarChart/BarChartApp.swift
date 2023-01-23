18f16bc7663490c0df49e21e61a397ff109c8551
Javlonbek <javlonbekdev@gmail.com>
2023-01-23 14:37:14 +0500

    Initial Commit

---
+++ BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Javlonbek on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
